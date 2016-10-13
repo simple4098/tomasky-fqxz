ddfe42c7f27b9273291b9e24a0a3633cb3ffbca7
xiezongming <xiezongming@671eefe9-2466-459f-b5fc-367f35298b91>
2016-10-13 06:59:18 +0000

    新小站

--- tomasky-fqxz/trunk/fqxz-web/src/main/java/com/tomasky/fqxz/web/ArticleController.java
+++ tomasky-fqxz/trunk/fqxz-web/src/main/java/com/tomasky/fqxz/web/ArticleController.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -30,20 +32,27 @@ public class ArticleController {
     @Autowired
     IArticleService articleService;
 
-    @ApiOperation(value = "查询文章攻略列表", notes = "查询文章攻略列表", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiImplicitParam(name = "param", value = "接收参数实体ArticleListBo", required = true, dataType = "ArticleListBo")
-    @RequestMapping(value = "/list", method = RequestMethod.POST)
-    public Map getList(
-            @RequestBody  ArticleListBo param) {
-        logger.info("查询文章攻略列表，参数" + JSON.toJSONString(param));
-        return articleService.getList(param);
+    @ApiOperation(value = "查询文章攻略列表", notes = "查询文章攻略列表", httpMethod = "GET")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public Map getList(@ApiParam(value = "客栈id",required = true) @RequestParam(value = "innId", name = "innId",required = true) Integer innId,
+                       @ApiParam(value = "页码",required = true) @RequestParam(value = "pageNo", name = "pageNo",required = true) Integer pageNo,
+                       @ApiParam(value = "条数",required = true) @RequestParam(value = "pageSize", name = "pageSize",required = true) Integer pageSize) {
+        ArticleListBo articleListBo=new ArticleListBo();
+        articleListBo.setInnId(innId);
+        articleListBo.setPageNo(pageNo);
+        articleListBo.setPageSize(pageSize);
+        logger.info("查询文章攻略列表，参数" + JSON.toJSONString(articleListBo));
+        return articleService.getList(articleListBo);
     }
 
 
-    @ApiOperation(value = "查询文章攻略详细", notes = "查询文章攻略详细", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiImplicitParam(name = "param", value = "接收参数实体ArticleDetailBo", required = true, dataType = "ArticleDetailBo")
-    @RequestMapping(value = "/detail", method = RequestMethod.POST)
-    public Map getDetail(@RequestBody  ArticleDetailBo param) {
+    @ApiOperation(value = "查询文章攻略详细", notes = "查询文章攻略详细", httpMethod = "GET")
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public Map getDetail(@ApiParam(value = "客栈id",required = true) @RequestParam(value = "innId", name = "innId",required = true) Long innId,
+                         @ApiParam(value = "文章id",required = true) @RequestParam(value = "articleId", name = "articleId",required = true) Long articleId) {
+        ArticleDetailBo param=new ArticleDetailBo();
+        param.setArticleId(articleId);
+        param.setInnId(innId);
         logger.info("查询文章攻略详细，参数" + JSON.toJSONString(param));
         return articleService.getDetail(param);
     }
